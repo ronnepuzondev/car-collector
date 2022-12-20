dcee971819b8381fabcd7acf37c30e3847ffe0ca
ronnepuzondev <116023509+ronnepuzondev@users.noreply.github.com>
2022-12-20 15:38:16 -0800

    add styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('accessories/<int:pk>/update/', views.AccessoryUpdate.as_view(), name='accessories_update'),
     path('accessories/<int:pk>/delete/', views.AccessoryDelete.as_view(), name='accessories_delete'),
     path('cars/<int:car_id>/assoc_accessory/<int:accessory_id>/', views.assoc_accessory, name='assoc_accessory'),
+    path('cars/<int:car_id>/remove_accessory/<int:accessory_id>/', views.remove_accessory, name='remove_accessory'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,7 @@ from .forms import MaintenanceForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Add the following import
 from django.http import HttpResponse
@@ -20,10 +21,12 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+
 @login_required
 def cars_index(request):
     cars = Car.objects.filter(user=request.user)
     return render(request, "cars/index.html", {"cars": cars})
+
 
 @login_required
 def cars_detail(request, car_id):
@@ -41,6 +44,7 @@ def cars_detail(request, car_id):
         },
     )
 
+
 @login_required
 def add_maintenance(request, car_id):
     # create a ModelForm instance using the data in request.POST
@@ -54,38 +58,46 @@ def add_maintenance(request, car_id):
         new_maintenance.save()
     return redirect("detail", car_id=car_id)
 
+
 @login_required
 def assoc_accessory(request, car_id, accessory_id):
     # Note that you can pass a toy's id instead of the whole toy object
     Car.objects.get(id=car_id).accessories.add(accessory_id)
     return redirect("detail", car_id=car_id)
 
+
+def remove_accessory(request, car_id, accessory_id):
+    Car.objects.get(id=car_id).accessories.remove(accessory_id)
+    return redirect("detail", car_id=car_id)
+
+
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ""
+    if request.method == "POST":
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect("index")
+        else:
+            error_message = "Invalid sign up - try again"
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
 
 
-class CarCreate(CreateView):
+class CarCreate(LoginRequiredMixin, CreateView):
     model = Car
-    fields = ['name', 'model', 'description', 'age']
+    fields = ["name", "model", "description", "age"]
     success_url = "/cars/"
+
     def form_valid(self, form):
-    # Assign the logged in user (self.request.user)
+        # Assign the logged in user (self.request.user)
         form.instance.user = self.request.user  # form.instance is the cat
         # Let the CreateView do its job as usual
         return super().form_valid(form)
