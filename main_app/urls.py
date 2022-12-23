6212fbe8305da7775a5f6b2a71f5b4db9795e58f
ronnepuzondev <116023509+ronnepuzondev@users.noreply.github.com>
2022-12-23 15:17:51 -0800

    deploy to github

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,18 +4,22 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # cars urls below
     path('cars/', views.cars_index, name='index'),
     path('cars/<int:car_id>/', views.cars_detail, name='detail'),
     path('cars/create/', views.CarCreate.as_view(), name='cars_create'),
     path('cars/<int:pk>/update/', views.CarUpdate.as_view(), name='cars_update'),
     path('cars/<int:pk>/delete/', views.CarDelete.as_view(), name='cars_delete'),
     path('cars/<int:car_id>/add_maintenance/', views.add_maintenance, name='add_maintenance'),
+    # accessories urls below
     path('accessories/', views.AccessoryList.as_view(), name='accessories_index'),
     path('accessories/<int:pk>/', views.AccessoryDetail.as_view(), name='accessories_detail'),
     path('accessories/create/', views.AccessoryCreate.as_view(), name='accessories_create'),
     path('accessories/<int:pk>/update/', views.AccessoryUpdate.as_view(), name='accessories_update'),
     path('accessories/<int:pk>/delete/', views.AccessoryDelete.as_view(), name='accessories_delete'),
+    #  to associate / add accessories urls below
     path('cars/<int:car_id>/assoc_accessory/<int:accessory_id>/', views.assoc_accessory, name='assoc_accessory'),
+    #  to remove accessories urls below
     path('cars/<int:car_id>/remove_accessory/<int:accessory_id>/', views.remove_accessory, name='remove_accessory'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
